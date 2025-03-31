9a14c567d25f8224de0d0a7900ed36cba139df61
Adelin Prelipcean <adelinprelipcean@users.noreply.github.com>
2025-03-31 21:39:01 +0300

    Adaugare Windows Forms la aplicatia existenta

---
+++ LMS_PIU_WinForms/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace LMS_PIU_WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.txtISBN = new System.Windows.Forms.TextBox();
+            this.txtCopies = new System.Windows.Forms.TextBox();
+            this.cmbCondition = new System.Windows.Forms.ComboBox();
+            this.cmbLevel = new System.Windows.Forms.ComboBox();
+            this.btnAddBook = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(289, 157);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(200, 20);
+            this.txtTitle.TabIndex = 6;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(289, 183);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(200, 20);
+            this.txtAuthor.TabIndex = 7;
+            // 
+            // txtISBN
+            // 
+            this.txtISBN.Location = new System.Drawing.Point(289, 209);
+            this.txtISBN.Name = "txtISBN";
+            this.txtISBN.Size = new System.Drawing.Size(200, 20);
+            this.txtISBN.TabIndex = 8;
+            // 
+            // txtCopies
+            // 
+            this.txtCopies.Location = new System.Drawing.Point(289, 235);
+            this.txtCopies.Name = "txtCopies";
+            this.txtCopies.Size = new System.Drawing.Size(200, 20);
+            this.txtCopies.TabIndex = 9;
+            // 
+            // cmbCondition
+            // 
+            this.cmbCondition.FormattingEnabled = true;
+            this.cmbCondition.Location = new System.Drawing.Point(289, 260);
+            this.cmbCondition.Name = "cmbCondition";
+            this.cmbCondition.Size = new System.Drawing.Size(150, 21);
+            this.cmbCondition.TabIndex = 10;
+            // 
+            // cmbLevel
+            // 
+            this.cmbLevel.FormattingEnabled = true;
+            this.cmbLevel.Location = new System.Drawing.Point(289, 287);
+            this.cmbLevel.Name = "cmbLevel";
+            this.cmbLevel.Size = new System.Drawing.Size(150, 21);
+            this.cmbLevel.TabIndex = 11;
+            // 
+            // btnAddBook
+            // 
+            this.btnAddBook.Location = new System.Drawing.Point(339, 314);
+            this.btnAddBook.Name = "btnAddBook";
+            this.btnAddBook.Size = new System.Drawing.Size(113, 23);
+            this.btnAddBook.TabIndex = 12;
+            this.btnAddBook.Text = "Adaugă Carte";
+            this.btnAddBook.UseVisualStyleBackColor = true;
+            this.btnAddBook.Click += new System.EventHandler(this.btnAddBook_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.ForeColor = System.Drawing.Color.Red;
+            this.lblMessage.Location = new System.Drawing.Point(385, 340);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblMessage.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(251, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Titlu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Autor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(251, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "ISBN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(234, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Cantitate";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(246, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Stare";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(188, 290);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Nivel Recomandat";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnAddBook);
+            this.Controls.Add(this.cmbLevel);
+            this.Controls.Add(this.cmbCondition);
+            this.Controls.Add(this.txtCopies);
+            this.Controls.Add(this.txtISBN);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txtTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.TextBox txtISBN;
+        private System.Windows.Forms.TextBox txtCopies;
+        private System.Windows.Forms.ComboBox cmbCondition;
+        private System.Windows.Forms.ComboBox cmbLevel;
+        private System.Windows.Forms.Button btnAddBook;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ LMS_PIU_WinForms/Form1.cs
@@ -0,0 +1,103 @@
+﻿using LMS_PIU;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LMS_PIU_WinForms
+{
+    public partial class Form1 : Form
+    {
+        private Library lib;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lib = new Library();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbCondition.DataSource = Enum.GetValues(typeof(BookCondition));
+            cmbLevel.DataSource = Enum.GetValues(typeof(EducationLevel));
+        }
+
+        private void btnAddBook_Click(object sender, EventArgs e)
+        {
+            string title = txtTitle.Text;
+            string author = txtAuthor.Text;
+            string isbn = txtISBN.Text;
+            int copies;
+
+            lblMessage.Text = "";
+            lblMessage.ForeColor = Color.Black;
+
+            if (!int.TryParse(txtCopies.Text, out copies) || copies < 1 || copies > 100)
+            {
+                lblMessage.Text = "Numărul de exemplare trebuie să fie între 1 și 100.";
+                lblMessage.ForeColor = Color.Red;
+                txtCopies.BackColor = Color.LightCoral;
+                return;
+            }
+            else
+            {
+                txtCopies.BackColor = Color.White;
+            }
+
+            if (string.IsNullOrWhiteSpace(title) || string.IsNullOrWhiteSpace(author))
+            {
+                lblMessage.Text = "Titlul și autorul sunt obligatorii.";
+                lblMessage.ForeColor = Color.Red;
+                txtTitle.BackColor = Color.LightCoral;
+                txtAuthor.BackColor = Color.LightCoral;
+                return;
+            }
+            else
+            {
+                txtTitle.BackColor = Color.White;
+                txtAuthor.BackColor = Color.White;
+            }
+
+            if (string.IsNullOrWhiteSpace(isbn))
+            {
+                lblMessage.Text = "ISBN-ul este obligatoriu.";
+                lblMessage.ForeColor = Color.Red;
+                txtISBN.BackColor = Color.LightCoral;
+                return;
+            }
+            else
+            {
+                txtISBN.BackColor = Color.White;
+            }
+
+            BookCondition condition = (BookCondition)cmbCondition.SelectedItem;
+            EducationLevel level = (EducationLevel)cmbLevel.SelectedItem;
+
+            try
+            {
+                Book newBook = new Book(title, author, isbn, copies, condition, level);
+                lib.AddBook(newBook);
+
+                lblMessage.Text = "Carte adăugată cu succes!";
+                lblMessage.ForeColor = Color.Green;
+
+                txtTitle.Clear();
+                txtAuthor.Clear();
+                txtISBN.Clear();
+                txtCopies.Clear();
+                txtTitle.BackColor = Color.White;
+                txtAuthor.BackColor = Color.White;
+                txtISBN.BackColor = Color.White;
+                txtCopies.BackColor = Color.White;
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = $"Eroare la adăugare: {ex.Message}";
+                lblMessage.ForeColor = Color.Red;
+            }
+        }
+    }
+}
